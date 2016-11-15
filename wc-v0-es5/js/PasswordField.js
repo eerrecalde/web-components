d1f0dde4e655005c0dcfac9528b7c6f994429641
Emiliano Errecalde <eerrecalde@gmail.com>
2016-11-15 16:50:52 +0000

    Added bower components to be reused

--- wc-v0-es5/js/PasswordField.js
+++ wc-v0-es5/js/PasswordField.js
@@ -1,166 +1,129 @@
-function InitPassField() {
+(function() {
 
-  class PassField extends HTMLElement {
+  var ownerDocument = (document._currentScript || document.currentScript).ownerDocument;
+  var template = ownerDocument.querySelector('#password-field-tpl');
 
-    constructor() {
 
-      // super();
-      //
-      // let shadowRoot = this.attachShadow({mode: 'open'});
-      // shadowRoot.innerHTML = `
-      //   <link rel="stylesheet" href="css/password-field.css">
-      //   <h2>Password input</h2>
-      //   <div class="ln-c-form-group">
-      //     <label for="demo-input-${this.getId()}" class="ln-c-label">
-      //       Label ${this.getId()}
-      //       <span class="ln-c-label__info" aria-hidden="true">*</span>
-      //       <span class="ln-c-label__info ln-u-visually-hidden">Required</span>
-      //     </label>
-      //     <div class="ln-c-form-password">
-      //       <input type="password" placeholder="Password" id="demo-input-${this.getId()}" class="ln-c-text-input" aria-describedby="password-more-info">
-      //       <button type="button" class="ln-js-show-hide ln-c-form-password__toggle-button ln-c-button ln-c-button--text-only">
-      //         <span class="ln-js-show-hide-text">Show</span> <span class="ln-u-visually-hidden">password</span>
-      //       </button>
-      //     </div>
-      //     <!-- Added wrapper for screen readers to read this whole element
-      //     as per aria-describedby in the input field indicate -->
-      //     <div id="password-more-info">
-      //
-      //       <div class="ln-c-password-strength-meter">
-      //         <p class="ln-c-password-strength-meter__label" aria-live="polite" aria-atomic="true">
-      //           Strength: <span class="ln-c-password-strength-meter__value">Too short</span>
-      //         </p>
-      //       </div>
-      //       <!-- Little hack to force screen readers to read this as it were a separate line.
-      //       Otherwise it reads the previous line and this once all in 1 -->
-      //       <p class="ln-u-visually-hidden">. To show and hide password, tab forward.</p>
-      //     </div>
-      //   </div>
-      // `;
+  var PassField = Object.create(HTMLElement.prototype);
+  PassField.createdCallback = function() {
+    this.init();
+    this.setAttrs();
+    this.addListeners();
+  };
+  PassField.init = function() {
+    this._root = this.createRootElement();
+  };
 
-      super(); // always call super() first in the ctor.
-      let shadowRoot = this.attachShadow({mode: 'open'});
-      const t = document.querySelector('#password-field');
-      const instance = t.content.cloneNode(true);
-      shadowRoot.appendChild(instance);
-
-      this.addListeners();
+  PassField.createRootElement = function() {
+    var root = this.createShadowRoot();
+    var content = document.importNode(template.content, true);
+    if (window.ShadowDOMPolyfill) {
+      WebComponents.ShadowCSS.shimStyling(content, 'passoword-field');
     }
+    root.appendChild(content);
+    return root;
+  };
 
-    get id() {
+  document.registerElement('password-field', {
+    prototype: PassField
+  });
+
+  Object.defineProperty(PassField, "id", {
+    get: function () {
       return this.getAttribute('id') || null;
-    }
-
-    set id(val) {
+    },
+    set: function (val) {
       if (val) {
         this.setAttribute('id', val);
       }
     }
+  });
 
-    getId() {
-      // Generate an id if it isn't already
-      if(!this.id) {
-        this.id = Math.floor((Math.random() * 100) + 1)
-      }
-
-      return this.id;
+  PassField.getId = function() {
+    // Generate an id if it isn't already
+    if(!this.id) {
+      this.id = Math.floor((Math.random() * 100) + 1)
     }
 
-    strengthMeterListener() {
-      const passwordEl = this.shadowRoot.querySelector('.ln-c-text-input')
-      const strengthMeterEl = this.shadowRoot.querySelector('.ln-c-password-strength-meter')
-      const strengthLevelTextEl = this.shadowRoot.querySelector('.ln-c-password-strength-meter__value')
+    return this.id;
+  }
 
-      // This meter level calculator is for demo purposes and should not be used in real cases.
-      const getLevel = (value) => {
-        let level = {}
-        if (value.length > 13) {
-          level = { level: 'is-level-4', caption: 'Great' }
-        } else if (value.length > 11) {
-          level = { level: 'is-level-3', caption: 'Good' }
-        } else if (value.length > 9) {
-          level = { level: 'is-level-2', caption: 'Ok' }
-        } else if (value.length > 7) {
-          level = { level: 'is-level-1', caption: 'Bad' }
-        } else {
-          level = { level: '', caption: 'Too short' }
-        }
-        return level
-      }
+  PassField.setAttrs = function() {
+    var label = this.shadowRoot.querySelector('label');
+    var labelTxt = this.shadowRoot.querySelector('.label-text');
+    var input = this.shadowRoot.querySelector('input[type="password"]');
 
-      passwordEl.onkeyup = () => {
-        const levelObject = getLevel(passwordEl.value)
+    label.setAttribute('for', 'demo-input-' + this.getId());
+    input.setAttribute('id', 'demo-input-' + this.getId());
+    labelTxt.innerText = 'Label ' + this.getId();
+  }
 
-        // Modify the strength level in text
-        strengthLevelTextEl.innerText = levelObject.caption
+  PassField.strengthMeterListener = function() {
+    var passwordEl = this.shadowRoot.querySelector('.ln-c-text-input')
+    var strengthMeterEl = this.shadowRoot.querySelector('.ln-c-password-strength-meter')
+    var strengthLevelTextEl = this.shadowRoot.querySelector('.ln-c-password-strength-meter__value')
 
-        // Update class names for the strength meter
-        if (strengthMeterEl) {
-          strengthMeterEl.className = `ln-c-password-strength-meter ${levelObject.level}`
-        }
-      }
-    }
-
-    showHidePasswordListener() {
-      const passwordEl = this.shadowRoot.querySelector('.ln-c-text-input')
-      const showHideLinkEl = this.shadowRoot.querySelector('.ln-js-show-hide')
-      const showHideTextEl = this.shadowRoot.querySelector('.ln-js-show-hide-text')
-
-      showHideLinkEl.onclick = (e) => {
-        e.preventDefault()
-        const attr = (passwordEl.getAttribute('type') === 'password' ? 'text' : 'password')
-        const linkText = (passwordEl.getAttribute('type') === 'password' ? 'Hide' : 'Show')
-
-        passwordEl.setAttribute('type', attr)
-        showHideTextEl.innerText = linkText
-      }
-    }
-
-    addListeners() {
-
-      // We either initialise the meter or delete the html related to it.
-      if (this.getAttribute('strength-meter') !== null && this.getAttribute('strength-meter') !== "false") {
-        this.strengthMeterListener();
+    // This meter level calculator is for demo purposes and should not be used in real cases.
+    var getLevel = function(value) {
+      var level = {}
+      if (value.length > 13) {
+        level = { level: 'is-level-4', caption: 'Great' }
+      } else if (value.length > 11) {
+        level = { level: 'is-level-3', caption: 'Good' }
+      } else if (value.length > 9) {
+        level = { level: 'is-level-2', caption: 'Ok' }
+      } else if (value.length > 7) {
+        level = { level: 'is-level-1', caption: 'Bad' }
       } else {
-        this.shadowRoot.querySelector('.ln-c-password-strength-meter').remove();
+        level = { level: '', caption: 'Too short' }
       }
+      return level
+    }
 
-      // We either initialise the show/hide or delete the html related to it.
-      if (this.getAttribute('show-hide-password') !== null && this.getAttribute('show-hide-password') !== "false") {
-        this.showHidePasswordListener();
-      } else {
-        this.shadowRoot.querySelector('.ln-c-form-password__toggle-button').remove();
+    passwordEl.onkeyup = function() {
+      var levelObject = getLevel(passwordEl.value)
+
+      // Modify the strength level in text
+      strengthLevelTextEl.innerText = levelObject.caption
+
+      // Update class names for the strength meter
+      if (strengthMeterEl) {
+        strengthMeterEl.className = 'ln-c-password-strength-meter ' + levelObject.level
       }
-
     }
   }
 
-  window.customElements.define('password-field', PassField);
+  PassField.showHidePasswordListener = function() {
+    var passwordEl = this.shadowRoot.querySelector('.ln-c-text-input')
+    var showHideLinkEl = this.shadowRoot.querySelector('.ln-js-show-hide')
+    var showHideTextEl = this.shadowRoot.querySelector('.ln-js-show-hide-text')
 
-}
+    showHideLinkEl.onclick = function(e) {
+      e.preventDefault()
+      var attr = (passwordEl.getAttribute('type') === 'password' ? 'text' : 'password')
+      var linkText = (passwordEl.getAttribute('type') === 'password' ? 'Hide' : 'Show')
 
-function loadScript(src) {
- return new Promise(function(resolve, reject) {
-   const script = document.createElement('script');
-   script.src = src;
-   script.onload = resolve;
-   script.onerror = reject;
-   document.head.appendChild(script);
- });
-}
+      passwordEl.setAttribute('type', attr)
+      showHideTextEl.innerText = linkText
+    }
+  }
 
-const supportsCustomElementsV1 = 'customElements' in window;
+  PassField.addListeners = function() {
 
-// Lazy load the polyfill if necessary.
-if (!supportsCustomElementsV1) {
-  console.log('Need Polyfill')
-  loadScript('../bower_components/custom-elements/custom-elements.min.js')
-    .then(e => loadScript('../bower_components/shadydom/shadydom.min.js'))
-    .then(e => {
-      console.log('Polyfill Added')
-      InitPassField()
-    });
-} else {
-  console.log('Polyfill Not Needed')
-  InitPassField()
-}
+    // We either initialise the meter or delete the html related to it.
+    if (this.getAttribute('strength-meter') !== null && this.getAttribute('strength-meter') !== "false") {
+      this.strengthMeterListener();
+    } else {
+      this.shadowRoot.querySelector('.ln-c-password-strength-meter').remove();
+    }
+
+    // We either initialise the show/hide or delete the html related to it.
+    if (this.getAttribute('show-hide-password') !== null && this.getAttribute('show-hide-password') !== "false") {
+      this.showHidePasswordListener();
+    } else {
+      this.shadowRoot.querySelector('.ln-c-form-password__toggle-button').remove();
+    }
+
+  }
+
+}())
