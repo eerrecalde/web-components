a206b5ee2ea5017ba08e357e22d26a6ccab32d6d
Emiliano Errecalde <eerrecalde@gmail.com>
2016-11-23 12:35:06 +0000

    Adding support for Safari on the component “wc-v1-es6”.

--- src/wc-v1-es6/js/PasswordField.js
+++ src/wc-v1-es6/js/PasswordField.js
@@ -5,10 +5,8 @@
 
       super(); // always call super() first in the ctor.
 
-      console.log('ownerDocument', document, document._currentScript, document.currentScript)
       const ownerDocument = (document._currentScript || document.currentScript).ownerDocument;
       const template = ownerDocument.querySelector('#password-field-tpl');
-
 
       let shadowRoot = this.attachShadow({mode: 'open'});
       const instance = template.content.cloneNode(true);
